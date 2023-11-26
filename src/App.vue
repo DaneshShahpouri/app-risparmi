26529921262333c101bbf208edc1489e0ea0d172
DaneshShahpouri <danesh.shahpouri@gmail.com>
2023-11-26 11:37:25 +0100

    scaffholding 3 - base

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 //import axios from 'axios';
 import { store } from './store.js';
 // import HelloWorld from './components/HelloWorld.vue'
+import AppMain from './components/AppMain.vue'
 
 export default {
   data() {
@@ -11,15 +12,30 @@ export default {
   },
 
   name: 'App',
+
   components: {
     // HelloWorld
-  }
+    AppMain,
+  },
+
+  methods: {
+
+  },
+
+  created() {
+
+  },
+
+  mounted() {
+
+  },
 }
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <AppMain></AppMain>
 </template>
 
 <style lang="scss" scoped></style>
---
+++ src/components/AppProva.vue
@@ -0,0 +1,33 @@
+<script>
+import { store } from '../store.js';
+export default {
+  name: 'AppProva',
+
+  data() {
+    return {
+      store,
+    }
+  },
+
+  props: {
+    // msg: String
+  },
+  methods: {
+
+  },
+
+  created() {
+
+  },
+
+  mounted() {
+
+  },
+}
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style lang="scss" scoped></style>
