16a21bd041dbdd6d1cc7b4e1a2ab3e60ebbaa589
DaneshShahpouri <danesh.shahpouri@gmail.com>
2023-11-26 11:31:58 +0100

    scaffholding 2 - vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import axios from 'axios';
+import { store } from './store.js';
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
+  data() {
+    return {
+      store,
+    }
+  },
+
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+</template>
+
+<style lang="scss" scoped></style>
